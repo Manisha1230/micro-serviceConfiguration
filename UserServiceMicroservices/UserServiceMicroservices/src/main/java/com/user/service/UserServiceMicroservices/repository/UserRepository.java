8458cbd4a36f5a5ed6565d51a8c5cf4d29423366
Manisha1230 <118426300+Manisha1230@users.noreply.github.com>
2023-10-16 22:46:10 +0530

    Delete UserServiceMicroservices/UserServiceMicroservices directory

--- UserServiceMicroservices/UserServiceMicroservices/src/main/java/com/user/service/UserServiceMicroservices/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.user.service.UserServiceMicroservices.repository;
-
-import com.user.service.UserServiceMicroservices.User.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity,String> {
-    // if we want to write any custom query or method
-
-}
